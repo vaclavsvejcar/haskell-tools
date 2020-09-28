3fef7f2ed33aaf90c9699ec7c85d6cc840a3cfe1
Vaclav Svejcar <vaclav.svejcar@gmail.com>
2020-09-28 20:59:43 +0200

    [mat35] Bump LTS Haskell to 16.16

--- respiwatch/src/RespiWatch/Crawler/RShield.hs
+++ respiwatch/src/RespiWatch/Crawler/RShield.hs
@@ -3,6 +3,17 @@ module RespiWatch.Crawler.RShield where
 
 import           RIO
 import           Text.HTML.Scalpel
+import           RespiWatch.Env                 ( HasPaths(..)
+                                                , Paths(..)
+                                                )
+import qualified RIO.Text                      as T
+
+allSoldOut :: (HasPaths env) => RIO env (Maybe Bool)
+allSoldOut = do
+  paths <- view pathsL
+  liftIO
+    $ scrapeURLWithConfig utf8Config (T.unpack . pRShieldURL $ paths) scraper
+  where scraper = undefined
 
 utf8Config :: Config Text
 utf8Config = Config utf8Decoder Nothing
